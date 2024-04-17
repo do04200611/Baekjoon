ffb9a65d54211b80e702395af619c46d57b75b58
kimganghyeon <kimganghyeon6080@naver.com>
2024-04-17 23:21:40 +0900

    Add files via upload

---
+++ 14681(사분면 고르기)/Baekjoon.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int x, y;
+	scanf("%d %d", &x, &y);
+
+		if (x < 0) { //x가 음수 일 때
+			if (y < 0) {// y가 음수 일 때
+				printf("3");
+			}
+			if (y > 0) {// y가 양수 일 때
+				printf("2");
+			}
+		}
+		else if (x > 0) { // x가 양수 일 때
+			if (y > 0) {// y가 양수 일 때
+				printf("1");
+			}
+			if (y < 0) { // y가 음수 일 때
+				printf("4");
+			}
+		}
+		return 0;
+
+		int x1, y1;
+		scanf("%d %d", &x1, &y1);
+
+		if (x1< 0) { //x가 음수 일 때
+			if (y1 < 0) {// y가 음수 일 때
+				printf("3");
+			}
+			if (y1> 0) {// y가 양수 일 때
+				printf("2");
+			}
+		}
+		else if (x1 > 0) { // x가 양수 일 때
+			if (y1 > 0) {// y가 양수 일 때
+				printf("1");
+			}
+			if (y1 < 0) { // y가 음수 일 때
+				printf("4");
+			}
+		}
+		return 0;
+	}
+
---
+++ 14681(사분면 고르기)/Main.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if ( x < 0) { //x가 음수 일 때
+			if (y < 0) {// y가 음수 일 때
+				System.out.println("3");
+			}
+			if (y>0) {// y가 양수 일 때
+				System.out.println("2");
+			}	
+		}
+		else if (x > 0) { // x가 양수 일 때
+			if (y>0) {// y가 양수 일 때
+				System.out.println("1");
+			}
+			if (y<0) { // y가 음수 일 때
+				System.out.println("4");
+			}
+		}
+		
+		
+		Scanner s = new Scanner(System.in);
+		int x1 = s.nextInt();
+		int y1 = s.nextInt();
+		
+		if ( x1 < 0) { //x가 음수 일 때
+			if (y1 < 0) {// y가 음수 일 때
+				System.out.println("3");
+			}
+			if (y1>0) {// y가 양수 일 때
+				System.out.println("2");
+			}	
+		}
+		else if (x1 > 0) { // x가 양수 일 때
+			if (y1>0) {// y가 양수 일 때
+				System.out.println("1");
+			}
+			if (y1<0) { // y가 음수 일 때
+				System.out.println("4");
+			}
+		}
+	}
+}
