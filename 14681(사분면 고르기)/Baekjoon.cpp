bb024593dcd472861afef3b6926bd7acc65d6687
kimganghyeon <kimganghyeon6080@naver.com>
2024-04-17 23:22:02 +0900

    Update Baekjoon.cpp

--- 14681(사분면 고르기)/Baekjoon.cpp
+++ 14681(사분면 고르기)/Baekjoon.cpp
@@ -23,25 +23,5 @@ int main() {
 		}
 		return 0;
 
-		int x1, y1;
-		scanf("%d %d", &x1, &y1);
-
-		if (x1< 0) { //x가 음수 일 때
-			if (y1 < 0) {// y가 음수 일 때
-				printf("3");
-			}
-			if (y1> 0) {// y가 양수 일 때
-				printf("2");
-			}
-		}
-		else if (x1 > 0) { // x가 양수 일 때
-			if (y1 > 0) {// y가 양수 일 때
-				printf("1");
-			}
-			if (y1 < 0) { // y가 음수 일 때
-				printf("4");
-			}
-		}
-		return 0;
 	}
 
