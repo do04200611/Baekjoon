ea4876034ea091edc9e49c6ceca947e010c07ecd
kimganghyeon <kimganghyeon6080@naver.com>
2024-04-19 23:40:31 +0900

    Update Main.java

--- 2480(주사위 세개)/Main.java
+++ 2480(주사위 세개)/Main.java
@@ -14,23 +14,23 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int A = 0, B =0 ,C=0, minute ;
+		int A, B, C ; // 변수 3개 선언
 		
-		A = sc.nextInt();
-		B = sc.nextInt();
-		C = sc.nextInt();
+		A = sc.nextInt(); // 입력 받은 값 중 1개 받음
+		B = sc.nextInt();//입력 받은 값 중 1개 받음
+		C = sc.nextInt();//입력 받은 값 중 1개 받음
 		
-		if (A==B&&B==C&&A==C) { 
+		if (A==B&&B==C&&A==C) {// 변수 모두 같을 때 
 			System.out.println(A*1000+10000);
 		} 
-		else if (A == B || A ==C ){ 
-			 System.out.println(Math.min(A,(Math.min(C, B)))*100+1000);
+		else if (A == B || A ==C ){ // 변수 2개가 같을 때
+			 System.out.println(A*100+1000);
 		}
-		else if (B==C) {
+		else if (B==C) {//변수 2개가 같을 때
 			System.out.println(B*100+1000);
 		}
-		else if (A !=B && B!=C&&A!=C) {
-			System.out.println(Math.max(A,(Math.max(C, B)))*100);
+		else if (A !=B && B!=C&&A!=C) {// 변수 모두 다를 때
+			System.out.println(Math.max(A,(Math.max(C, B)))*100); // 최솟값 최댓 값 구하기
 		}
 	}
 }
