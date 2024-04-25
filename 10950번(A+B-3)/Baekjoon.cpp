2ca7477938c2515aa10d6e94bbda7f261bd1951d
kimganghyeon <kimganghyeon6080@naver.com>
2024-04-25 23:34:51 +0900

    Add files via upload

---
+++ 10950번(A+B-3)/Baekjoon.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d", &a);
+
+	for (int i = 0; i != a; i++)
+	{
+		int  ac, ab;
+		scanf("%d %d",  &ac, &ab);
+		printf("%d",ac+ab );
+
+	}
+
+}
