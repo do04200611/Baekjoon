720f59b870d07d9611cafd565a6acfc3430a1547
kimganghyeon <kimganghyeon6080@naver.com>
2024-04-27 19:36:03 +0900

    Create BufferReader.java

---
+++ 개수 세기(10807)/BufferReader.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+public class BufferReader {
+   public static void main(String args[])throws IOException{
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //BufferedReader 객체화 함
+	   BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));// BufferedWriter 객체화 함
+	   
+	   int a = Integer.parseInt(br.readLine());//a를 변수로 선언
+	   int ar[] = new int[a];//ar 배열을 객체화함
+	   int count =0;// 변수 count를 0으로 초기화함
+
+	   StringTokenizer  st = new StringTokenizer(br.readLine());//StringTokenizer를 객체화함
+
+		for (int i = 0; i <ar.length; i++) {//i가 배열 ar의 길이보다 클 때 까지 
+			ar[i]=Integer.parseInt(st.nextToken());// 배열ar에 요소를 입력받음
+		}
+		int x =  Integer.parseInt(br.readLine());//값 x를 입력받음
+		
+
+		for (int i = 0; i <ar.length; i++) { //i가 배열의 크기가 클 때 까지
+			if (ar[i] == x) {// 배열의 요소 입력 받은 x값하고 같으면
+				count++; // count 값을 1씩 더함
+			}
+		}
+		bw.write(count+"\n");//변수 count 값 출력
+		bw.flush();
+		bw.close();
+
+   }
+}
