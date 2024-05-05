d02228a6985f8bba68a2944076e352b8a3d79866
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-05 21:04:59 +0900

    Update Main.java

--- 단어 길이 재기(2743)/Main.java
+++ 단어 길이 재기(2743)/Main.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class Main{
 	public static void main(String[] args)throws Exception{
 			
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		System.out.println(s.length());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//BufferedReader를 객체화함
+		String s = br.readLine();//변수 s에 값 입력 받음
+		System.out.println(s.length());//s에 저장된 값의 길이 출력
 	}
 }
