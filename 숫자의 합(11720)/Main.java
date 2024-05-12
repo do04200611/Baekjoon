18741dbbf98d9c8e47c59405bbba96bd48380894
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-12 22:23:47 +0900

    Update Main.java

--- 숫자의 합(11720)/Main.java
+++ 숫자의 합(11720)/Main.java
@@ -19,38 +19,5 @@ public class Main{
 		}
 	System.out.println(sum);
 	
-	Scanner sc2 = new Scanner(System.in);//Scanner를 객체화		
-	int a2 = sc2.nextInt();//a 값을 입력받음
-	String s2 =sc2.next();//s의 값을 입력받음
-	int sum2 =0;//sum을 초기화함
-	
-	for (int i = 0; i < a2; i++) {//i를 0부터 해서 a보다 작을 때 까지
-	sum2 += s2.charAt(i)-'0';//s를 int
-
-	}
-System.out.println(sum2);
-
-Scanner sc3 = new Scanner(System.in);//Scanner를 객체화		
-int a3 = sc3.nextInt();//a 값을 입력받음
-String s3 =sc3.next();//s의 값을 입력받음
-int sum3 =0;//sum을 초기화함
-
-for (int i = 0; i < a3; i++) {//i를 0부터 해서 a보다 작을 때 까지
-sum3 += s3.charAt(i)-'0';//s를 int
-
-}
-System.out.println(sum3);
-
-Scanner sc33 = new Scanner(System.in);//Scanner를 객체화		
-int a33 = sc33.nextInt();//a 값을 입력받음
-String s33 =sc33.next();//s의 값을 입력받음
-int sum33 =0;//sum을 초기화함
-
-for (int i = 0; i < a33; i++) {//i를 0부터 해서 a보다 작을 때 까지
-sum33 += s33.charAt(i)-'0';//s를 int
-
-}
-System.out.println(sum33);
-
 	}
 }
