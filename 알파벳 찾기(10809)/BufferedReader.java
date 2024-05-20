f8efdb4c59958c4e838e8b4f3e772b90386c684f
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-20 21:31:20 +0900

    Update BufferedReader.java

--- 알파벳 찾기(10809)/BufferedReader.java
+++ 알파벳 찾기(10809)/BufferedReader.java
@@ -1,23 +1,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		String s = br.readLine();
-		int array[] = new int[26];
-		Arrays.fill(array, -1); // 전부 다 -1로 초기화 
-		 for (int i = 0; i < s.length(); i++) {
-			 char ch = s.charAt(i); //ch
-				if(array[ch - 'a']==-1) { //-1인 경우에만 수정 (처음 등장하는 위치 조건)  
-					array[ch-'a'] = i;  //array 배열 요소의 값을 i로 초기화함
-				}
+			for (char c = 'a' ; c <= 'z'; c++) {
+					sb.append(s.indexOf(c)+" "); // 문자열의 위치를 반환,
 			}
-		 for(int i=0;i<array.length;i++) {
-				System.out.print(array[i]+" ");
-			}
+			System.out.println(sb);
 		}
 	}
