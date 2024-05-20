3b04cd8336f8bbd9a2fb5584c5c4e9d82162d3e4
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-20 10:29:48 +0900

    Update Main.java

--- 1152(단어의 개수)/Main.java
+++ 1152(단어의 개수)/Main.java
@@ -1,23 +1,12 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
-
 
 public class Main{
 	public static void main(String[] args)throws Exception{
 		
-		Scanner sc = new Scanner(System.in);
-		StringTokenizer st2 = new StringTokenizer(sc.nextLine()," ");
-		System.out.println(st2.countTokens());
+		Scanner sc = new Scanner(System.in); //Scanner를 객체화함
+		StringTokenizer st = new StringTokenizer(sc.nextLine()," ");//문자를 입력 받으면서, 공백으로 구분자로 설정함
+		System.out.println(st.countTokens());//st의 개수를 출력
 		
 	}
 }
