0acb83e981c795c92193ec8fc4550f5aa553ef7e
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-20 10:18:53 +0900

    Add files via upload

---
+++ 1152(단어의 개수)/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+
+public class Main{
+	public static void main(String[] args)throws Exception{
+		
+		Scanner sc = new Scanner(System.in);
+		StringTokenizer st2 = new StringTokenizer(sc.nextLine()," ");
+		System.out.println(st2.countTokens());
+		
+	}
+}
