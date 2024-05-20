c81c8a06818606914e27134886f86b68614eb425
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-20 11:34:00 +0900

    Update Main.java

--- 1152(단어의 개수)/Main.java
+++ 1152(단어의 개수)/Main.java
@@ -6,7 +6,7 @@ public class Main{
 		
 		Scanner sc = new Scanner(System.in); //Scanner를 객체화함
 		StringTokenizer st = new StringTokenizer(sc.nextLine()," ");//문자를 입력 받으면서, 공백으로 구분자로 설정함
-		System.out.println(st.countTokens());//st의 개수를 출력
+		System.out.println(st.countTokens());//입력 받은 토큰의 개수를 출력
 		
 	}
 }
