9278b65ba93d2ea4fd1190b21dbdc5001fb7d10b
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-27 23:15:33 +0900

    Update Baekjoon.cpp

--- 기타 단계/N 찍기(2741)/Baekjoon.cpp
+++ 기타 단계/N 찍기(2741)/Baekjoon.cpp
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-	int num;
-	scanf("%d", &num);
+	int num; //num을 변수를 int형으로 선언
+	scanf("%d", &num); //num의 값을 입력 받음
 
-	for (int i = 1; i <= num; i++) {
-		printf("%d\n", i);
+	for (int i = 1; i <= num; i++) { //i가 1부터 num보다 크거나 같을 때 까지 실행
+		printf("%d\n", i); //i의 값을 출력
 	}
 
 	
