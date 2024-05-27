cfb4beec9838d9bb5f744a1f936ca889d4f66ec7
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-27 23:14:17 +0900

    Update Main.java

--- 기타 단계/N 찍기(2741)/Main.java
+++ 기타 단계/N 찍기(2741)/Main.java
@@ -10,11 +10,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         
-    	Scanner sc = new Scanner(System.in);
-    	int num = sc.nextInt();
+    	Scanner sc = new Scanner(System.in); //Scanner 객체화함
+    	int num = sc.nextInt(); //num의 값을 입력 받음
     	
-    	for (int i = 1; i <= num; i++) {
-			System.out.println(i);
+    	for (int i = 1; i <= num; i++) { //i가 1부터 num보다 크거나 같을 때 까지 실행
+			System.out.println(i); // i의 값 출력
 		}
     }
 
