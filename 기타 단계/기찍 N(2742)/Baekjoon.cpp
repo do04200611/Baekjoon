5c9b5dcb48cd6211330c3f816d21fea4c14f7f9e
kimganghyeon <kimganghyeon6080@naver.com>
2024-05-27 23:19:11 +0900

    Update Baekjoon.cpp

--- 기타 단계/기찍 N(2742)/Baekjoon.cpp
+++ 기타 단계/기찍 N(2742)/Baekjoon.cpp
@@ -4,12 +4,12 @@
 
 int main(void)
 {
-	int num;
-	scanf("%d", &num);
-	int a = num;
-	for (int i = 0; i < num; i++) {
-		printf("%d\n", a);
-		a--;
+	int num; //변수 num을 int형으로 선언함
+	scanf("%d", &num); //num의 값을 입력 받음
+	int a = num; //a가 num에 대입함
+	for (int i = 0; i < num; i++) { //i가 0부터 num보다 작거나 같을 때 까지 실행함
+		printf("%d\n", a); //a의 값 출력
+		a--; //a값 감소
 
 	}
 
