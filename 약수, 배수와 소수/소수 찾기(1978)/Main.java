7c99f3ad5c7b61cd65f59bcf7d369d3f5c36b342
kimganghyeon <kimganghyeon6080@naver.com>
2024-06-13 17:15:25 +0900

    Update Main.java

--- 약수, 배수와 소수/소수 찾기(1978)/Main.java
+++ 약수, 배수와 소수/소수 찾기(1978)/Main.java
@@ -13,19 +13,19 @@ public class Main {
          
          int isPN, count = 0; //숫자와 소수의 개수를 파악할 변수 2개 선언
          
-         for (int i = 0; i < num; i++) {
-        	 isPN = Integer.parseInt(st.nextToken());
-			for (int j = 2; j <= isPN; j++) {
-				if (j == isPN) {
-					count++;
+         for (int i = 0; i < num; i++) {//i가 0부터 num보다 작을 때까지 실행
+        	 isPN = Integer.parseInt(st.nextToken());//숫자 입력 받음
+			for (int j = 2; j <= isPN; j++) {//2부터 시작해서 입력 받은 숫자 보다 크거나 같을 때 까지 실행
+				if (j == isPN) {//j가 입력 받은 숫자와 같을 때
+					count++;//개수 증가
 				}
-				if (isPN % j == 0) {
-					break;
+				if (isPN % j == 0) {//입력받은 수중에서 약수가 있을 때
+					break; // 종료
 
 				}
 			}
 			}
-     	System.out.println(count); 
+     	System.out.println(count); //개수 출력
 
 	}
 }
