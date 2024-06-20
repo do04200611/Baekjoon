105208ada718b574f66fc00adbcbf903b36327a0
kimganghyeon <kimganghyeon6080@naver.com>
2024-06-20 17:00:28 +0900

    Update Main.java

--- 기하: 직사각형과 삼각형/네 번째 점(3009)/Main.java
+++ 기하: 직사각형과 삼각형/네 번째 점(3009)/Main.java
@@ -7,22 +7,21 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //BufferedReader 객체화
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); //BufferedWriter 객체화
 		
-		StringTokenizer st;
+		StringTokenizer st; 
 		
-		int x[] = new int[3];
-		int y[] = new int[3];
+		int x[] = new int[3]; //배열 x를 객체화함
+		int y[] = new int[3]; //배열 y를 객체화함
 
-		for (int i = 0; i < 3; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			x[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < 3; i++) { //i가 0부터 3보다 작을 때 까지 실행함
+			st = new StringTokenizer(br.readLine(), " "); 
+			x[i] = Integer.parseInt(st.nextToken()); 
 			y[i] = Integer.parseInt(st.nextToken());
-//			System.out.println(x[i]+" "+y[i]);
 		}
 		
-		int other_x = 0;
+		int other_x = 0; 
 		int other_y =0;
 		
 		if (x[0] == x[1]) {
@@ -35,6 +34,6 @@ public class Main {
 		}else {
 			other_y =(y[0] == y[2]) ? (y[1]) : (y[0]);
 		}
-		System.out.println(other_x+" "+ other_y);
+		System.out.println(other_x+" "+ other_y); // 네 번째 점 출력
 	}
 }
